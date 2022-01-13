685a605460613ac7b62c0b1a412c3e2e9048a6b2
Criz <crizmo595@gmail.com>
2022-01-13 09:59:59 +0530

    Create add.java

---
+++ base/add.java
@@ -0,0 +1,13 @@
+package base;
+
+public class add {
+    public static void main(String[] args) {
+
+        int a = 3, b = 5 ;
+        int sum = 0;
+        sum = a + b;
+
+        System.out.println("Answer is " + sum);
+        System.out.println("Addition of "+ a + " and "+ b +" is "+ sum);  // or instead of sum you can write +(a+b)
+    }
+}
