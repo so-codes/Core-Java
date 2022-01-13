34203ab6f03f26ae6aa08a957c1e5042f7e3cbda
Criz <crizmo595@gmail.com>
2022-01-13 13:53:53 +0530

    Create div_5.java

---
+++ base/div_5.java
@@ -0,0 +1,19 @@
+package base;
+import java.util.*;
+
+public class div_5 {
+    public static void main(String[] args) throws Exception{
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter number: ");
+            int num = sc.nextInt();
+
+            if(num % 5 == 0){
+                System.out.println("Number is divisible by 5");
+            }
+            else {
+                System.out.println("Number is not divisible by 5");
+            }
+        }
+    }
+}
