77673074ac6ec3a2847c7c5bf624e4157963a73e
Criz <crizmo595@gmail.com>
2022-01-13 10:56:17 +0530

    Create pos_neg.java

---
+++ base/pos_neg.java
@@ -0,0 +1,19 @@
+package base;
+import java.util.*; 
+
+public class pos_neg {
+    public static void main(String[] args) throws Exception{
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter number: ");
+            int num = sc.nextInt(); 
+
+            if(num >= 0){
+                System.out.println("Positive");
+            }
+            else {
+                System.out.println("Negative");
+            }
+        }
+    }
+}
