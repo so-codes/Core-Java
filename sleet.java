1b4bd1b0d2d4a186f0c9d20b46bad38903f50503
Criz <crizmo595@gmail.com>
2022-01-13 09:05:14 +0530

    start

---
+++ sleet.java
@@ -0,0 +1,5 @@
+public class sleet {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Criz");
+    }
+}
---
+++ yeet.java
@@ -0,0 +1,8 @@
+// java [Package].[ClassName]
+
+class yeet {
+    public static void main(String[] args){
+        String x = "Criz";
+        System.out.println(x);
+    }
+}
