3ab16f79e10c99fab22d706181315770b4a01798
Criz <crizmo595@gmail.com>
2022-01-13 14:35:53 +0530

    Update area.java

--- base/area.java
+++ base/area.java
@@ -6,7 +6,7 @@ public class area {
         int l = 10, b = 5 , area = l * b, r = 3;
         final float pi = 3.14f;
 
-        System.out.println("\nArea of a rectangle when lengeth is "+ l +" and breadth is "+ b);
+        System.out.println("\nArea of a rectangle when length is "+ l +" and breadth is "+ b);
         System.out.println("\nArea of a rectangle is " + area);
         System.out.println("\nWhen radius = "+ r + "\nArea of a circle is " + (pi*r*r));
         System.out.print("\n");
