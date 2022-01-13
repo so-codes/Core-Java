85a6cbf64f06fd553b035ffdaa2c0ce54390c69f
Criz <crizmo595@gmail.com>
2022-01-13 13:42:22 +0530

    changes + add & avg

--- base/add.java
+++ base/add.java
@@ -4,8 +4,7 @@ public class add {
     public static void main(String[] args) {
 
         int a = 3, b = 5 ;
-        int sum = 0;
-        sum = a + b;
+        int sum = a + b;
 
         System.out.println("Answer is " + sum);
         System.out.println("Addition of "+ a + " and "+ b +" is "+ sum);  // or instead of sum you can write +(a+b)
---
+++ base/sum_avg.java
@@ -0,0 +1,21 @@
+package base;
+import java.util.*;
+
+public class sum_avg {
+    public static void main(String[] args) throws Exception{
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("\nEnter three numbers: ");
+            
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            int num3 = sc.nextInt();
+
+            int sum , avg;
+            sum = num1 + num2 + num3;
+            avg = sum / 3;
+
+            System.out.print("\n\nSum of three numbers: "+ sum +"\nAverage of three numbers: "+ avg +"\n\n");
+        }
+    }
+}
