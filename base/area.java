c2e240e786017f6a51e5bce810de5bf6e231f0f0
Criz <crizmo595@gmail.com>
2022-01-13 10:27:58 +0530

    Create area.java

---
+++ base/area.java
@@ -0,0 +1,19 @@
+package base;
+
+public class area {
+    public static void main(String[] args) {
+
+        int l = 10, b = 5 , area;
+        System.out.println("\nArea of a rectangle when lengeth is "+ l +" and breadth is "+ b);
+
+        area = l * b;
+
+        System.out.println("\nArea of a rectangle is " + area);
+
+        final float pi = 3.14f;
+        int r = 3;
+
+        System.out.println("\nWhen radius = "+ r + "\nArea of a circle is " + (pi*r*r));
+        System.out.print("\n");
+    }
+}
