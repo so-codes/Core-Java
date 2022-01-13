e6af85ba12c96ae92db9340f75f0bdfcb1cb0759
Criz <crizmo595@gmail.com>
2022-01-13 10:16:00 +0530

    Create swap.java

---
+++ base/swap.java
@@ -0,0 +1,16 @@
+package base;
+
+public class swap {
+    
+    public static void main(String[] args) {
+
+        int a = 5, b = 10, c;
+        System.out.print("\nThe two numbers are a = "+ a +" b = "+ b +" - Before swapping\n");
+
+        c = a;  
+        a = b;  
+        b = c; 
+
+        System.out.print("\nThe two numbers are a = "+ a +" b = "+ b +" - After swapping\n\n");
+    }
+}
