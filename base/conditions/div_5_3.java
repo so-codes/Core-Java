521c610a1005af80ec754c40a8037380f5d698d1
Criz <crizmo595@gmail.com>
2022-01-17 10:46:09 +0530

    poggers

---
+++ base/conditions/div_5_3.java
@@ -0,0 +1,28 @@
+package base.conditions;
+import java.util.*;
+
+public class div_5_3 {
+    public static void main(String[] args) throws Exception{
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter number: ");
+            int num = sc.nextInt();
+
+            if (num % 3 == 0 && num % 5 == 0){
+                System.out.println("Number is divisible by 3 & 5 both");
+            }
+            else if (num % 3 != 0 && num % 5 != 0){
+                System.out.println("Number is divisible by 3 & 5");
+            }
+            else if (num % 3 == 0){
+                System.out.println("Number is divisible by 3");
+            }
+            else if(num % 5 == 0){
+                System.out.println("Number is divisible by 5");
+            }
+            else{
+                System.out.println("Number is not divisible by 3 & 5");
+            }
+        }
+    }
+}
