06ce01a9f117f5d4d61c77cff796fbf1cc2f7c54
Criz <crizmo595@gmail.com>
2022-01-17 11:07:05 +0530

    Create input.java

---
+++ base/basics/input.java
@@ -0,0 +1,14 @@
+package base.basics;
+import java.util.*;
+
+public class input {
+        public static void main(String[] args) throws Exception{
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter number:- ");
+            double poggers = sc.nextDouble(); 
+
+            System.out.println("User num is: " + poggers);
+        }
+    }
+}
