c8a61a6ed0b1b4d062077e7f78a8de0f9feb814f
Criz <crizmo595@gmail.com>
2022-01-17 10:20:48 +0530

    Create casting.java

---
+++ base/casting/casting.java
@@ -0,0 +1,24 @@
+package base.casting;
+
+public class casting {
+    public static void main(String[] args){
+
+        // int to double
+
+        int sleet = 10;
+        double yeet = sleet;  // automatic casting
+
+        System.out.println(sleet);
+        System.out.println(yeet);
+
+        System.out.print("\n");
+
+        // double to int
+
+        double dou = 5.5;
+        int inn = (int) dou;  // manual casting
+
+        System.out.println(dou);
+        System.out.println(inn);
+    }
+}
