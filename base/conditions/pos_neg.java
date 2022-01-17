ee1735a96159c73fcbe4d1e443be0e314550eed6
Criz <crizmo595@gmail.com>
2022-01-17 10:50:57 +0530

    Update pos_neg.java

--- base/conditions/pos_neg.java
+++ base/conditions/pos_neg.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class pos_neg {
     public static void main(String[] args) throws Exception{
 
-        try (Scanner sc = new Scanner(System.in)) {
+        try (Scanner pog = new Scanner(System.in)) {
             System.out.print("Enter number: ");
-            int num = sc.nextInt(); 
+            int num = pog.nextInt(); 
 
             if(num > 0){
                 System.out.println("Positive");
