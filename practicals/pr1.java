7afe9a7acbff68d6a21b70069ec914f08b9c78c1
ErenWNL <94686773+ErenWNL@users.noreply.github.com>
2022-01-20 13:49:31 +0530

    Create pr1.java

---
+++ practicals/pr1.java
@@ -0,0 +1,13 @@
+package practicals;
+
+public class pr1 {
+        
+        public static void main(String[] args){
+        String x = "Hello";
+        System.out.println(x);
+        System.out.println("My name is Srihari");
+
+    }
+    
+}
+    
