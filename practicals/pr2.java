c3ed3d9ca0ba8e432c1d30e5c9103b50372784b0
Criz <crizmo595@gmail.com>
2022-01-20 19:30:33 +0530

    Update pr2.java

--- practicals/pr2.java
+++ practicals/pr2.java
@@ -5,22 +5,30 @@ public class pr2 {
     
     public static void main(String[] args){
         try (Scanner sc = new Scanner(System.in)){
-            System.out.print("Enter name: ");
+            System.out.print("\nEnter name: ");
             String str= sc.nextLine();
             System.out.println("Name: "+str);
-            System.out.print("Enter gender: ");
+
+            System.out.print("\nEnter gender: ");
             char x = sc.next().charAt(0);
             System.out.println("Gender: "+x);
-            System.out.print("Enter the age: ");
+
+            System.out.print("\nEnter the age: ");
             int n = sc.nextInt();
             System.out.println("The age is: "+n);
-            System.out.print("Enter the phone no: ");
+
+            System.out.print("\nEnter the phone no: ");
             long a = sc.nextLong();
             System.out.println("Phone No: "+a);
-            System.out.print("Enter the percentage: ");
+
+            System.out.print("\nEnter the percentage: ");
             float b = sc.nextFloat();
             System.out.println("Percentage: "+b);
-           
-         }
+
+            System.out.print("\nEnter double percent: ");
+            double per_dou = sc.nextDouble();
+            int per_int = (int) per_dou;
+            System.out.println("Converting double % to int % : "+ per_int);
+        }
     }
 }
