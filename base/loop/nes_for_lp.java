fa00f61d480609b177bf20674fe5457523701ad9
Criz <crizmo595@gmail.com>
2022-01-20 09:46:47 +0530

    Update nes_for_lp.java

--- base/loop/nes_for_lp.java
+++ base/loop/nes_for_lp.java
@@ -9,12 +9,31 @@ public class nes_for_lp {
             int n = sc.nextInt();
             System.out.println("Pattern");
 
-            for(int i = 1;i<=n;i++){
-                for(int j = 1;j<=i;j++){
+            for(int i = 1;i <= n;i++){
+                for(int j = 1;j <= i;j++){
                     System.out.print("*"+" ");
                 }
                 System.out.println();
             }
+            System.out.println();
+
+            for(int i = 1;i <= n;i++){
+                for(int j = 1;j <= i;j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            int count = 1;
+            for(int i = 1;i <= n;i++){
+                for(int j = 1;j <= i;j++){
+                    System.out.print(count+" ");
+                    count++;
+                }
+                System.out.println();
+            }
+            System.out.println();
     }
     }
 }
