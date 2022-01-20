3326fb2bb6d8830b636f325e2b3bfdb511d201df
Criz <crizmo595@gmail.com>
2022-01-20 10:07:02 +0530

    Create arrbase.java

---
+++ base/array/arrbase.java
@@ -0,0 +1,31 @@
+package base.array;
+import java.util.*;
+
+public class arrbase {
+    public static void main(String[] args) {
+        
+        int age[] = {1,2,3,4,5};
+        age[0] = 5;
+        age[4] = 10;
+        
+        for(int i = 0;i<5;i++){
+            System.out.println("Age at index "+ i +": "+ age[i]);
+        }
+
+        System.out.println();
+
+        int a[] = new int[5];
+        try (Scanner pog = new Scanner(System.in)) {
+            for(int i = 0;i<5;i++){
+                System.out.print("Enter number: ");
+                a[i] = pog.nextInt();
+            }
+            
+            System.out.println();
+
+            for(int i = 0;i<5;++i){
+                System.out.println("Elements at index "+ i +": "+ a[i]);
+            }
+        }
+    }
+}
