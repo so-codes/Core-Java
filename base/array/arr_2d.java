06682ed6807cc29c3e4b3781d5f6d545a2c0a5b5
Criz <crizmo595@gmail.com>
2022-01-20 10:51:59 +0530

    Create arr_2d.java

---
+++ base/array/arr_2d.java
@@ -0,0 +1,28 @@
+package base.array;
+import java.util.*;
+
+public class arr_2d {
+    public static void main(String[] args) {
+        try (Scanner pog = new Scanner(System.in)) {
+            int z[][] = new int[3][3];
+
+            System.out.println();
+
+            for(int i = 0;i<3;i++){     // rows
+                for(int j = 0;j<3;j++){   // columns
+                    System.out.print("Enter number at pos "+ i +" "+ j+": ");
+                    z[i][j] = pog.nextInt();
+                }
+            }
+            System.out.println();
+
+            for(int i = 0;i<3;i++){     // rows
+                for(int j = 0;j<3;j++){   // columns
+                    System.out.print(z[i][j]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
