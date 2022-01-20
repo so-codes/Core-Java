d2533ce63fee99947202416631e0842a073f5cae
ErenWNL <94686773+ErenWNL@users.noreply.github.com>
2022-01-20 14:09:55 +0530

    Update input.java

--- base/basics/input.java
+++ base/basics/input.java
@@ -2,7 +2,7 @@ package base.basics;
 import java.util.*;
 
 public class input {
-        public static void main(String[] args) throws Exception{
+        public static void main(String[] args){
         
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter number:- ");
