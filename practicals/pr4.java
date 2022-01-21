b28d1f61b6f2b508fce1c772e228660a68c629bd
Criz <crizmo595@gmail.com>
2022-01-21 12:18:19 +0530

    Update pr4.java

--- practicals/pr4.java
+++ practicals/pr4.java
@@ -1,13 +1,17 @@
 package practicals;
+import java.util.*;
 
 public class pr4 {
     public static void main(String[] args) {
-        final int radius = 18;
+        try (Scanner sc = new Scanner(System.in)){
+            System.out.print("\nEnter radius: ");
+            int radius = sc.nextInt();
 
-        double perimeter = 2 * Math.PI * radius;
-        double area = Math.PI * radius * radius;
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * radius * radius;
 
-        System.out.println("Perimeter is = " + perimeter);
-        System.out.println("Area is = " + area);
+            System.out.println("Perimeter is = " + perimeter);
+            System.out.println("Area is = " + area);
+        }
     }
 }
