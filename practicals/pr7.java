2d2946e60c181aefbc5845be685a6f8f0bb4cb45
Criz <crizmo595@gmail.com>
2022-01-21 12:37:17 +0530

    6 & 7

--- practicals/pr7.java
+++ practicals/pr7.java
@@ -4,18 +4,20 @@ public class pr7 {
     public static void main(String[] args){
     try (Scanner sc = new Scanner(System.in)){
 
-        System.out.print("Enter the age: ");
+        System.out.print("\nEnter the age: ");
         int age = sc.nextInt();
-        int x;
 
-        if (age >= 18 && age < 100){
-          System.out.print("User is eligible to vote");
+        if (age >= 18 && age <= 100){
+          System.out.print("\nUser is eligible to vote\n\n");
+        }
+        else if(age > 100){
+          System.out.print("\nUser is eligible to vote , Please take care !\n\n");
         }
 
         else{
-          System.out.println("User is not eligible to vote");
-          x = 18 - age;
-          System.out.println("Number of years left to vote: "+x);
+          System.out.print("\nUser is not eligible to vote\n");
+          int x = 18 - age;
+          System.out.println("\nNumber of years left to vote: "+ x +"\n");
         }
       }
     } 
