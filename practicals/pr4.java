d3d4602ee7dea4ce614265499ce8c3610267bbab
Criz <crizmo595@gmail.com>
2022-01-21 12:26:01 +0530

    Update pr4.java

--- practicals/pr4.java
+++ practicals/pr4.java
@@ -5,7 +5,7 @@ public class pr4 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)){
             System.out.print("\nEnter radius: ");
-            int radius = sc.nextInt();
+            double radius = sc.nextDouble();
 
             final double PI = 3.14159;
 
