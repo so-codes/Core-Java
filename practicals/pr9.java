7e71fc873e2cb4fc3c48d24ead02a7c68ed070c5
Criz <crizmo595@gmail.com>
2022-01-24 10:12:35 +0530

    Update pr9.java

--- practicals/pr9.java
+++ practicals/pr9.java
@@ -4,27 +4,33 @@ import java.util.*;
 public class pr9 {
     public static void main(String args[]) {
     try (Scanner sc = new Scanner(System.in)) {
+
         System.out.print("\nEnter starting num: ");
         int start = sc.nextInt();
 
         System.out.print("\nEnter ending number: ");
         int end = sc.nextInt();
+
         int i = 0 ;
-    
-        System.out.println("\nDisplay the even numbers between "+ start +" and "+ end +" are :");
 
-        for(i = start; i <= end; i++){ 
-            int r = i % 2; 
-            if(r == 0){
-                System.out.println(i);
-            }
+        if(start > end){
+            System.out.println("The starting number should be smaller than the end number");
         }
+        else {
+            System.out.println("\nDisplay the even numbers between "+ start +" and "+ end +" are :");
+            for(i = start; i <= end; i++){ 
+                int r = i % 2; 
+                if(r == 0){
+                    System.out.println(i);
+                }
+            }
 
-        System.out.println("\nDisplay the odd numbers between "+ start +" and "+ end +" are :");
-        for(i = start; i <= end; i++){ 
-            int r = i % 2; 
-            if(r == 1){ 
-                System.out.println(i);
+            System.out.println("\nDisplay the odd numbers between "+ start +" and "+ end +" are :");
+            for(i = start; i <= end; i++){ 
+                int r = i % 2; 
+                if(r == 1){ 
+                    System.out.println(i);
+                }
             }
         }
     }
