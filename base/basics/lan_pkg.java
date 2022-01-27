ec6850c66eb0bf7e6e6f604e7260cfe2e17fff86
Criz <crizmo595@gmail.com>
2022-01-27 09:51:16 +0530

    Create lan_pkg.java

---
+++ base/basics/lan_pkg.java
@@ -0,0 +1,11 @@
+package base.basics;
+
+public class lan_pkg {
+    public static void main(String[] args){
+
+        System.out.println(Math.abs(-12));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.pow(2, 2));
+    }
+}
