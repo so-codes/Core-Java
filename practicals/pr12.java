7c2e342c47aa61577c86897b97e0d54b88b122dd
Criz <crizmo595@gmail.com>
2022-01-27 12:14:36 +0530

    Create pr12.java

---
+++ practicals/pr12.java
@@ -0,0 +1,35 @@
+package practicals;
+import java.util.*;
+
+public class pr12 {
+    static boolean isArmstrong(int n) {
+        int temp, digits = 0, last = 0, sum = 0;
+        temp = n;
+        while (temp > 0) {
+            temp = temp / 10;
+            digits++;
+        }
+        temp = n;
+        while (temp > 0) {
+            last = temp % 10;
+            sum += (Math.pow(last, digits));
+            temp = temp / 10;
+        }
+        if (n == sum)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        num = sc.nextInt();
+        if (isArmstrong(num)) {
+            System.out.print("Entered number is an Armstrong ");
+        } else {
+            System.out.print("Entered number is Not an Armstrong ");
+        }
+        sc.close();
+    }
+}
