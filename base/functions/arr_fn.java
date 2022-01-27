24fc3458216f2c5ee01d594f4c4151ec13de3444
Criz <crizmo595@gmail.com>
2022-01-27 09:46:53 +0530

    Update arr_fn.java

--- base/functions/arr_fn.java
+++ base/functions/arr_fn.java
@@ -16,24 +16,24 @@ class Account1
 	}
 	 void showData()
 	{
-		System.out.println("Emp ID ="+a);
-		System.out.println("Emp Sal ="+b);
+		System.out.println("Emp ID = " + a);
+		System.out.println("Emp Sal = " + b);
 	}
 }
 
 public class arr_fn {
-    public static void main(String args[])
+    public static void main(String[] args)
 	{	
 		Account1 obj[] = new Account1[4];
 
-		for(int i=0;i<4;i++)
+		for(int i = 0 ; i < 4  ;i++)
 		{
 			//obj0=new Account1();
-			obj[i]=new Account1();
+			obj[i] = new Account1();
 			obj[i].setData();
 			//obj0.setData();
 		}
-		for(int i=0;i<4;i++)
+		for(int i = 0; i < 4; i++)
 		{
 			obj[i].showData();
 		}
