1062d9791fd88827004651241c8035e9fd3701fd
Criz <crizmo595@gmail.com>
2022-01-27 13:32:05 +0530

    calc

--- practicals/pr14.java
+++ practicals/pr14.java
@@ -1,15 +1,17 @@
-package base.conditions;
+package practicals;
 import java.util.*;
 
-public class sw_add_sub {
+public class pr14 {
     public static void main(String[] args) throws Exception{
 
         try (Scanner pog = new Scanner(System.in)) {
+            char ch = 'y';
+            while(ch =='y'){
             System.out.print("\nEnter two number: ");
             int a = pog.nextInt();
             int b = pog.nextInt();
 
-            System.out.print("\n+ = Addition\n- = Substraction\n* = Multiplication\n/ = Multiplication\n");
+            System.out.print("\nChoose an operation\n+ = Addition\n- = Substraction\n* = Multiplication\n/ = Multiplication\n------------------------------\n");
 
             System.out.print("\nEnter an operation: ");
             char op = pog.next().charAt(0);
@@ -33,8 +35,10 @@ public class sw_add_sub {
 
                 default:
                     System.out.println("\nIncorrect operation");
-                    break;
-            }
+            } 
+            System.out.println("Do you want to continue: ");
+            ch = pog.next().charAt(0);    
         }
+    }
     }
 }
