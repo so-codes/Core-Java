43951d28f7e9f343597a3b9fb0ad46a4dfc1cfb7
Criz <crizmo595@gmail.com>
2022-01-31 10:51:28 +0530

    Update overload_calc.java

--- base/basics/overload_calc.java
+++ base/basics/overload_calc.java
@@ -2,7 +2,7 @@ package base.basics;
 
 public class overload_calc {
     void display(){
-        System.out.println("This is method overloading program");
+        System.out.println("This is method overloading program\n");
     }
     void display(int a , int b){
         System.out.println("Addition of int a & b: " + (a+b));
@@ -10,11 +10,17 @@ public class overload_calc {
     void display(double a , double b){
         System.out.println("Addition of double a & b: " + (a+b)); 
     }
-
+    void display(String a , String b, String c){
+        System.out.println("Addition of string: " + (a+b+c)); 
+    }
     public static void main(String[] args){
         overload_calc m1 = new overload_calc();
+        System.out.print("\n");
         m1.display();
+        
         m1.display(20,30);
         m1.display(10.5,20.5);
+        m1.display("Criz"," is ","poggers");
+        System.out.print("\n");
     }
 }
