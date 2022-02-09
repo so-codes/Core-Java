a82f098707b4a96acc94b140755f719693710d86
Criz <crizmo595@gmail.com>
2022-02-09 11:07:58 +0530

    update super

--- base/class_fn/extends_fn.java
+++ base/class_fn/extends_fn.java
@@ -1,17 +1,18 @@
 package base.class_fn;
 
 class extends_fn {
-    extends_fn(int x){
+    extends_fn(int x, int y){
         System.out.println("A is called "+ x);
     }
 }
 
 class B1 extends extends_fn {
-    B1(int x){
-        super(x);
-        System.out.println("B1 is called " + x);
+    int z;
+    B1(int x, int y , int z){
+        super(x , y);
+        System.out.println("B1 is called " + z);
     }
     public static void main(String[] args) {
-        new B1(3);    // or B1 b1 = new B1();
+        new B1(3 , 2 , 4);    // or B1 b1 = new B1();
     }   
 }
