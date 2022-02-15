8615827e19c35b9d9735c55750358727bde7c447
Criz <crizmo595@gmail.com>
2022-02-15 10:59:41 +0530

    Update basic.java

--- base/interface_bs/basic.java
+++ base/interface_bs/basic.java
@@ -3,20 +3,22 @@ package base.interface_bs;
 interface basic {
     public void print();
 }
+interface basic1 {
+    public void print();
+}
 
-class A implements basic {
+class A implements basic, basic1 {
 
     public void print() {
         System.out.println("This is A");
     }
-    void methods(){
-        System.out.println("This is A's methods");
+    public void print1() {
+        System.out.println("This is A1");
     }
-
 
     public static void main(String[] args) {
         A a = new A();
         a.print();
-        a.methods();
+        a.print1();
     }
 }
