dd1217e9bd512d1ff8ea427841758f1696220a52
Criz <crizmo595@gmail.com>
2022-02-16 11:15:09 +0530

    Create music_fo.java

---
+++ base/interface_bs/music_fo.java
@@ -0,0 +1,25 @@
+package base.interface_bs;
+
+interface guitar {
+    public void play();
+}
+
+interface guitar_player extends guitar {
+    public void player();
+}
+
+public class music_fo {
+    public static void main(String[] args) {
+        guitar_player g = new guitar_player() {
+            public void play() {
+                System.out.println("Guitar is playing");
+            }
+            public void player() {
+                System.out.println("Guitar player");
+            }
+        };
+        g.play();
+        g.player();
+    }
+    
+}
