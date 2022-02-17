18fff67cc5cb246a9d8be9247bc9e5863e245ea6
Criz <crizmo595@gmail.com>
2022-02-17 10:38:40 +0530

    Create subTst.java

---
+++ base/packages/add_pkg/subTst.java
@@ -0,0 +1,14 @@
+package base.packages.add_pkg;
+
+public class subTst {
+    double x, y;
+
+    public subTst(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double sub() {
+        return x + y;
+    }
+}
