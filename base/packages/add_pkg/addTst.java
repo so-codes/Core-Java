f94df8904f66f11574ac4ed32c572751da3f5aed
Criz <crizmo595@gmail.com>
2022-02-17 10:38:38 +0530

    Create addTst.java

---
+++ base/packages/add_pkg/addTst.java
@@ -0,0 +1,14 @@
+package base.packages.add_pkg;
+
+public class addTst {
+    double x, y;
+
+    public addTst(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double sum() {
+        return x + y;
+    }
+}
