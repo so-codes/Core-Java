8aad91c7ebf05366ab212059a2d17c10e09dcd2c
Criz <crizmo595@gmail.com>
2022-02-17 10:14:51 +0530

    Update CubeTst.java

--- base/packages/CubeTst.java
+++ base/packages/CubeTst.java
@@ -18,6 +18,22 @@ public class CubeTst {
                 System.out.println("Cube of " + n + " is " + c.cube(n));
             }
             System.out.println();
+
+            factorial f = new factorial();
+            for (int i = 0; i < t; i++) {
+                System.out.print("Enter the number: ");
+                int n = sc.nextInt();
+                System.out.println("Factorial of " + n + " is " + f.fact(n));
+            }
+            System.out.println();
+
+            cubeclass s = new cubeclass();
+            for (int i = 0; i < t; i++) {
+                System.out.print("Enter the number: ");
+                int c1 = sc.nextInt();
+                System.out.println("Cube of " + c1 + " is " + s.square(c1));
+            }
+            System.out.println();
         }
     }
 }
