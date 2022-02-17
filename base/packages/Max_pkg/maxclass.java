c0d513209cfa928bb208e2145ed76ce336369f25
Criz <crizmo595@gmail.com>
2022-02-17 10:15:03 +0530

    Create maxclass.java

---
+++ base/packages/Max_pkg/maxclass.java
@@ -0,0 +1,13 @@
+package base.packages.Max_pkg;
+
+public class maxclass {
+    public double max(double a, double b) {
+        double max = 0;
+        if (a > b) {
+            max = a;
+        } else {
+            max = b;
+        }
+        return max;
+    }
+}
