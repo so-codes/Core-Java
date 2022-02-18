c760a4f79132c987d320ff817512cf75e1e76da6
Criz <crizmo595@gmail.com>
2022-02-18 13:27:51 +0530

    Create pr25.java

---
+++ practicals/pr25.java
@@ -0,0 +1,27 @@
+package practicals;
+
+import java.util.*;
+
+public class pr25 {
+
+    public static void main(String[] args) {
+
+        try (Scanner pog = new Scanner(System.in)) {
+            System.out.print("\nEnter two numbers: ");
+            int a = pog.nextInt();
+            int b = pog.nextInt();
+
+            int max = max(a, b);
+            System.out.println("Maximum number is: " + max + "\n");
+        }
+    }
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+}
