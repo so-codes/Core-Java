ef305a1d7b91d9d6ef1b73fb5343c57054409230
Criz <crizmo595@gmail.com>
2022-02-22 10:13:10 +0530

    Update even_arr.java

--- base/array/even_arr.java
+++ base/array/even_arr.java
@@ -1,4 +1,5 @@
 package base.array;
+
 import java.util.*;
 
 public class even_arr {
@@ -6,14 +7,14 @@ public class even_arr {
         try (Scanner pog = new Scanner(System.in)) {
             int z[] = new int[5];
 
-            for(int i = 0;i<5;i++){
+            for (int i = 0; i < 5; i++) {
                 System.out.print("Enter number: ");
                 z[i] = pog.nextInt();
             }
             System.out.println("\nEven numbers in the array:");
-            for(int i = 0;i<5;i++){
-                if(z[i] % 2 == 0){
-                    System.out.print(z[i]+ " ");
+            for (int i = 0; i < 5; i++) {
+                if (z[i] % 2 == 0) {
+                    System.out.print(z[i] + " ");
                 }
             }
         }
