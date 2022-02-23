659c3bba16cad86513c73213318afa47b7aeb0f3
Criz <crizmo595@gmail.com>
2022-02-23 08:39:21 +0530

    Update calc_in_fn.java

--- yeet/calc_in_fn.java
+++ yeet/calc_in_fn.java
@@ -5,18 +5,23 @@ import java.util.*;
 class calc_in_fn {
     Scanner sc;
     int a;
-    void accept(){
+
+    void accept() {
         sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         a = sc.nextInt();
     }
 }
 
+// extends is used to get the variables from the parent class
+// and the implements is used to get the methods from the interface
+
 class cube extends calc_in_fn {
-    void calcube(){
-        System.out.println("Cube of number is: " + (a*a*a));
+    void calcube() {
+        System.out.println("Cube of number is: " + (a * a * a));
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         cube c = new cube();
         c.accept();
         c.calcube();
