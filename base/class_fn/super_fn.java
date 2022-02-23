0144cf3b0a710f2d02a8ca9893a5383fd191d205
Criz <crizmo595@gmail.com>
2022-02-23 08:33:23 +0530

    Update super_fn.java

--- base/class_fn/super_fn.java
+++ base/class_fn/super_fn.java
@@ -1,13 +1,15 @@
 package base.class_fn;
 
 class super_fn {
-    int x , y , z;
-    super_fn(int x , int y , int z){
+    int x, y, z;
+
+    super_fn(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
-    void display(){
+
+    void display() {
         System.out.println("x = " + x);
         System.out.println("y = " + y);
         System.out.println("z = " + z);
@@ -16,19 +18,21 @@ class super_fn {
 
 class Bsub extends super_fn {
     int a, b;
-    Bsub(int x , int y , int z , int a , int b){
-        super(x , y , z);
+
+    Bsub(int x, int y, int z, int a, int b) {
+        super(x, y, z);
         this.a = a;
         this.b = b;
     }
-    void show(){
+
+    void show() {
         System.out.println("a = " + a + " b = " + b);
         super.display();
         System.out.println("a = " + super.x);
     }
 
     public static void main(String[] args) {
-        Bsub b1 =new Bsub(1,2,3,4,5);
+        Bsub b1 = new Bsub(1, 2, 3, 4, 5);
         b1.show();
     }
 }
