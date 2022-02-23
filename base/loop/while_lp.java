5998e930b464fd6c1bae88df2f4e34fb31e95374
Criz <crizmo595@gmail.com>
2022-02-23 08:36:56 +0530

    Update while_lp.java

--- base/loop/while_lp.java
+++ base/loop/while_lp.java
@@ -1,26 +1,29 @@
 package base.loop;
+
 import java.util.*;
 
 public class while_lp {
-    
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
 
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter number: ");
             int num = sc.nextInt();
 
-            System.out.print("First "+ num + " natural numbers");
             int i = 1;
+
+            System.out.print("First " + num + " natural numbers");
             System.out.println("1");
-            while(i <= num){
+
+            while (i <= num) {
                 System.out.println(i + " ");
                 i = i + 1;
             }
 
-            System.out.print("First "+ num + " natural numbers in reverse order");
+            System.out.print("First " + num + " natural numbers in reverse order");
             System.out.println("5");
 
-            while(num > 0){
+            while (num > 0) {
                 System.out.println(num);
                 num -= 1;
             }
