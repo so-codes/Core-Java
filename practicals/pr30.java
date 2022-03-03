0164922517ee14eb2f7bc82a7b881103fb773fdf
Criz <crizmo595@gmail.com>
2022-03-03 13:31:43 +0530

    Create pr30.java

---
+++ practicals/pr30.java
@@ -0,0 +1,50 @@
+package practicals;
+
+import java.util.*;
+
+class number {
+    int num1;
+    int num2;
+
+    number(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    void display5() {
+        System.out.println("\nNumber 1: " + num1 + "\nNumber 2: " + num2);
+    }
+}
+
+class CAdd extends number {
+    CAdd(int num1, int num2) {
+        super(num1, num2);
+        System.out.println("\nAddition of " + num1 + " and " + num2 + " is " + (num1 + num2));
+    }
+}
+
+class CSub extends CAdd {
+    CSub(int num1, int num2) {
+        super(num1, num2);
+        System.out.println("\nSubtraction of " + num1 + " and " + num2 + " is " + (num1 - num2));
+    }
+}
+
+public class pr30 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("\n");
+
+            System.out.print("Enter the number 1: ");
+            int num1 = sc.nextInt();
+            System.out.print("Enter the number 2: ");
+            int num2 = sc.nextInt();
+
+            // new CAdd(num1, num2);
+            new CSub(num1, num2);
+
+        }
+    }
+
+}
