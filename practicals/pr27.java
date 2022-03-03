d04fb6dedc761b96b2ec3016f0a3b756c9ddcfd4
Criz <crizmo595@gmail.com>
2022-03-03 13:31:35 +0530

    Create pr27.java

---
+++ practicals/pr27.java
@@ -0,0 +1,33 @@
+package practicals;
+
+import java.util.*;
+
+public class pr27 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("\n");
+
+            System.out.print("Enter the number: ");
+            int num = sc.nextInt();
+            pr27 obj = new pr27(num);
+            obj.display();
+        }
+    }
+
+    int num;
+    int fact = 1;
+
+    pr27(int num) {
+        this.num = num;
+    }
+
+    void display() {
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        System.out.println("\nFactorial of " + num + " is " + fact + "\n");
+    }
+
+}
