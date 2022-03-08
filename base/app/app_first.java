348970f2d35fc945c4d815e87d986b199ef29eb3
Criz <83665497+crizmo@users.noreply.github.com>
2022-03-08 17:43:26 +0530

    Create app_first.java

---
+++ base/app/app_first.java
@@ -0,0 +1,25 @@
+package base.app;
+import javax.swing.*;
+import java.awt.*;
+
+public class app_first extends JFrame {
+
+    JLabel l1;
+    app_first() {
+
+        l1 = new JLabel("hello");
+        add(l1);
+
+        setSize(200,200);
+        setVisible(true);
+        setLayout(new FlowLayout());
+        setTitle("Poggers");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        new app_first();
+    }
+    
+}
