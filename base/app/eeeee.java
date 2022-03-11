a18845fe99c775d9273a8593ee2f19d43fd99b7d
Criz <crizmo595@gmail.com>
2022-03-11 17:52:23 +0530

    Create eeeee.java

---
+++ base/app/eeeee.java
@@ -0,0 +1,18 @@
+package base.app;
+
+public class eeeee {
+    public static void main(String[] args) {
+
+        double a, b;
+        float ans;
+
+        // int, long, short, byte, char, boolean, float, double
+
+        a = 22;
+        b = 7;
+        ans = (float) (a / b);
+
+        System.out.println(ans);
+    }
+
+}
