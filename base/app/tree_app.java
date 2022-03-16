b4f78273b95a961cd937ef713878cdee87914f39
Criz <83665497+crizmo@users.noreply.github.com>
2022-03-16 14:42:39 +0530

    Create tree_app.java

---
+++ base/app/tree_app.java
@@ -0,0 +1,40 @@
+package base.app;
+
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+
+public class tree_app extends JFrame {
+    
+    tree_app(){
+
+            DefaultMutableTreeNode it = new DefaultMutableTreeNode("root");
+            DefaultMutableTreeNode fy = new DefaultMutableTreeNode("FY");
+            DefaultMutableTreeNode sy = new DefaultMutableTreeNode("SY");
+            DefaultMutableTreeNode child3 = new DefaultMutableTreeNode("child3");
+
+            it.add(fy);
+            it.add(sy);
+            it.add(child3);
+
+            DefaultMutableTreeNode w = new DefaultMutableTreeNode("web");
+            DefaultMutableTreeNode c = new DefaultMutableTreeNode("c++");
+
+            sy.add(w);
+            sy.add(c);
+
+            JTree tree = new JTree(it);
+            add(new JScrollPane(tree));
+
+            setSize(400,200);
+            setVisible(true);
+            setLayout(new FlowLayout());
+            setTitle("Poggers");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new tree_app();
+    }
+}
