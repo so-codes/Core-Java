d76cd52a87322aded0c4f3b064d5ed94c93bc4f2
Criz <crizmo595@gmail.com>
2022-03-30 19:03:09 +0530

    Update pr2.java

--- practicals/pr2.java
+++ practicals/pr2.java
@@ -1,56 +1,54 @@
 package practicals;
+
 import java.util.*;
 
 public class pr2 {
-    
-    public static void main(String[] args){
-        try (Scanner sc = new Scanner(System.in)){
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
             System.out.print("\nEnter name: ");
             String str = sc.nextLine().toLowerCase();
 
             int cr = 1;
             int cr1 = 2;
-            if(cr < cr1){
-                String pog = "criz";
+            if (cr < cr1) {
+                String pog = "tejas";
                 String pog2 = "srihari";
 
-                if(str.equalsIgnoreCase(pog) || str.equalsIgnoreCase(pog2)){
-                    System.out.println("O.o its "+ str);
-                }
-                else{
-                    System.out.println("Name: "+str);
+                if (str.equalsIgnoreCase(pog) || str.equalsIgnoreCase(pog2)) {
+                    System.out.println("O.o its " + str);
+                } else {
+                    System.out.println("Name: " + str);
                 }
             }
-            
+
             System.out.print("\nEnter gender: ");
             char x = sc.next().charAt(0);
 
-            if(x == 'F' || x == 'f'){
+            if (x == 'F' || x == 'f') {
                 System.out.println("User gender is female");
-            }
-            else if(x == 'M' || x == 'm'){
+            } else if (x == 'M' || x == 'm') {
                 System.out.println("User gender is male");
-            }
-            else{
+            } else {
                 System.out.println("Invalid gender");
             }
 
             System.out.print("\nEnter the age: ");
             int n = sc.nextInt();
-            System.out.println("The age is: "+n);
+            System.out.println("The age is: " + n);
 
             System.out.print("\nEnter the phone no: ");
             long a = sc.nextLong();
-            System.out.println("Phone No: "+a);
+            System.out.println("Phone No: " + a);
 
             System.out.print("\nEnter the percentage: ");
             float b = sc.nextFloat();
-            System.out.println("Percentage: "+b);
+            System.out.println("Percentage: " + b);
 
             System.out.print("\nEnter double percent: ");
             double per_dou = sc.nextDouble();
             int per_int = (int) per_dou;
-            System.out.println("Converting double % to int % : "+ per_int);
+            System.out.println("Converting double % to int % : " + per_int);
         }
     }
 }
