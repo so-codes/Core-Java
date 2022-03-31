ec565df67a469dae10f48729bd39bf0c2edaac56
Criz <83665497+crizmo@users.noreply.github.com>
2022-03-31 10:03:41 +0530

    Update tree_ex1.java

--- base/app/tree_ex1.java
+++ base/app/tree_ex1.java
@@ -2,11 +2,19 @@ package base.app;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 
-public class tree_ex1 extends JFrame {
+public class tree_ex1 extends MouseAdapter {
+
+    JLabel content;
+    JTextField text;
+    JTree tree;
 
     tree_ex1(){
+
+        JFrame f = new JFrame();
 
         DefaultMutableTreeNode style  = new DefaultMutableTreeNode("Style");
         DefaultMutableTreeNode color = new DefaultMutableTreeNode("Color");
@@ -23,14 +31,40 @@ public class tree_ex1 extends JFrame {
         color.add(blue);
         color.add(green);
 
-        JTree tree = new JTree(style);
-        add(new JScrollPane(tree));
+        DefaultMutableTreeNode serif = new DefaultMutableTreeNode("Serif");
+        DefaultMutableTreeNode sansSerif = new DefaultMutableTreeNode("SansSerif");
+        DefaultMutableTreeNode monospaced = new DefaultMutableTreeNode("Monospaced");
 
-        setSize(400,200);
-        setVisible(true);
-        setLayout(new FlowLayout());
-        setTitle("Poggers");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        font.add(serif);
+        font.add(sansSerif);
+        font.add(monospaced);
+
+        tree = new JTree(style);
+        f.add(new JScrollPane(tree));
+
+        content = new JLabel("");
+        text = new JTextField(50);
+
+        // f.add(content);
+        f.add(text);
+
+        tree.addMouseListener(this);
+
+        f.setSize(400,400);
+        f.setVisible(true);
+        f.setLayout(new GridLayout(2,2));
+        f.setTitle("Poggers");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void mouseClicked(MouseEvent e){
+        TreePath path = tree.getPathForLocation(e.getX(), e.getY());
+        if(path == null){
+            text.setText("None obj selected");
+        }
+        if(path != null){
+            text.setText(path.toString());
+        }
     }
 
     public static void main(String[] args) {
