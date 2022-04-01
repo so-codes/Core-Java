2da7f4aa88c9d3b2f22929859d311a8d9ab9aa19
Criz <crizmo595@gmail.com>
2022-04-01 17:43:12 +0530

    Update pr6.java

--- practicals/pr6.java
+++ practicals/pr6.java
@@ -2,11 +2,11 @@ package practicals;
 
 public class pr6 {
 
-public static void main(String[] args) {
-    System.out.println("Even Numbers between 100 and 200: ");
-        for(int i = 100;i <= 200;i++){
-            if(i % 2 == 0){
-                System.out.print(i +" ");
+    public static void main(String[] args) {
+        System.out.println("Even Numbers between 100 and 200: ");
+        for (int i = 100; i <= 200; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
             }
         }
     }
