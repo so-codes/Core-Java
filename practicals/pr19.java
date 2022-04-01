f4f4301645b1bd2e0daa42927683f637cf7be7b5
Criz <crizmo595@gmail.com>
2022-04-01 17:43:10 +0530

    Update pr19.java

--- practicals/pr19.java
+++ practicals/pr19.java
@@ -3,12 +3,13 @@ package practicals;
 import java.util.Scanner;
 
 public class pr19 {
-    // addition of two binary numbers
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+
             System.out.print("\nEnter first binary number: ");
             int num1 = sc.nextInt();
+
             System.out.print("\nEnter second binary number: ");
             int num2 = sc.nextInt();
 
@@ -16,12 +17,12 @@ public class pr19 {
             int rem = 0;
 
             while (num1 > 0 || num2 > 0) {
-                rem = (num1 % 10) + (num2 % 10) + carry;
-                carry = rem / 2;
-                rem = rem % 2;
-                System.out.print(rem);
-                num1 = num1 / 10;
-                num2 = num2 / 10;
+                rem = (num1 % 10) + (num2 % 10) + carry; // ( 101 % 10 ) + ( 100 % 10 ) + 0
+                carry = rem / 2; // ( 101 % 10 ) + ( 100 % 10 ) + 0 / 2 = 1
+                rem = rem % 2; // 1 % 2 = 1
+                System.out.print(rem); // 1
+                num1 = num1 / 10; // 101 / 10 = 10
+                num2 = num2 / 10; // 100 / 10 = 10
             }
         }
         System.out.print("\n\n");
