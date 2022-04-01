ae887332cdf922a1d3c80f3b7d33a08f1ca96d4e
Criz <crizmo595@gmail.com>
2022-04-01 17:43:08 +0530

    Update pr12.java

--- practicals/pr12.java
+++ practicals/pr12.java
@@ -1,4 +1,5 @@
 package practicals;
+
 import java.util.*;
 
 public class pr12 {
@@ -20,12 +21,13 @@ public class pr12 {
         else
             return false;
     }
+
     public static void main(String[] args) {
         int num;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         num = sc.nextInt();
-        
+
         if (isArmstrong(num)) {
             System.out.print("Entered number is an Armstrong ");
         } else {
