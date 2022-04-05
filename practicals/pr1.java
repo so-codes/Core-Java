ffbaca3100dffc513e00c234dc2ebc32ff853a97
Criz <crizmo595@gmail.com>
2022-04-05 20:27:59 +0530

    🎈 : minor spacing

--- practicals/pr1.java
+++ practicals/pr1.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class pr1 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+
             System.out.print("\nEnter your name: ");
             String str = sc.nextLine();
 
