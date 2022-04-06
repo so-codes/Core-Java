79c01e15821d99ea7ab234457e04931fc1db233f
Criz <crizmo595@gmail.com>
2022-04-06 20:33:10 +0530

    🎈: spacing

--- base/class_fn/abstract_fn.java
+++ base/class_fn/abstract_fn.java
@@ -5,32 +5,36 @@ abstract class abstract_fn {
 }
 
 class add extends abstract_fn {
-    int a,b;
-    add(int a,int b){
-        this.a=a;
-        this.b=b;
+    int a, b;
+
+    add(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
-    void display(){
-        System.out.println(a+b);
+
+    void display() {
+        System.out.println(a + b);
     }
 }
 
 class sub extends abstract_fn {
-    int a,b;
-    sub(int a,int b){
-        this.a=a;
-        this.b=b;
+    int a, b;
+
+    sub(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
-    void display(){
-        System.out.println(a-b);
+
+    void display() {
+        System.out.println(a - b);
     }
 }
 
 class MainCal {
     public static void main(String[] args) {
-        add obj1=new add(10,20);
+        add obj1 = new add(10, 20);
         obj1.display();
-        sub obj2=new sub(10,20);
+        sub obj2 = new sub(10, 20);
         obj2.display();
     }
 }
