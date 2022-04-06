725a4eae03ce5d9f295bb0072932136b4f986016
Criz <crizmo595@gmail.com>
2022-04-06 15:51:30 +0530

    Update pr20.java

--- practicals/pr20.java
+++ practicals/pr20.java
@@ -9,13 +9,17 @@ public class pr20 {
         int sum = 0;
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("\nEnter 5 nos. in array: ");
+
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = sc.nextInt();
             }
+
         }
+
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
+
         System.out.println("\nSum of all elements in array: " + sum);
     }
 }
