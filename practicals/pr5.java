203f6487dbb97ff7618cc2a3a6d6d116940243a5
Criz <crizmo595@gmail.com>
2022-04-06 14:56:48 +0530

    🎈: scanner er

--- practicals/pr5.java
+++ practicals/pr5.java
@@ -1,13 +1,17 @@
 package practicals;
+
 import java.util.*;
 
 public class pr5 {
-    public static void main (String[] args) {  
-        System.out.print("\nEnter hours: ");
-        try (Scanner sc = new Scanner(System.in)){  
-        int hours = sc.nextInt(); 
-        int minutes = hours * 60; 
-        System.out.println("There are " + minutes + " minutes in " + hours + " hours\n");   
-        }  
-    }  
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("\nEnter hours: ");
+            int hours = sc.nextInt();
+
+            int minutes = hours * 60;
+            System.out.println("There are " + minutes + " minutes in " + hours + " hours\n");
+        }
+    }
 }
