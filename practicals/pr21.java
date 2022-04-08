bc3f3779c0c313905fc127dc6ecdd781f5cd56e4
Criz <crizmo595@gmail.com>
2022-04-08 08:48:54 +0530

    Update pr21.java

--- practicals/pr21.java
+++ practicals/pr21.java
@@ -1,20 +1,60 @@
 package practicals;
 
+import java.util.Scanner;
+
 public class pr21 {
 
     public static void main(String args[]) {
 
-        int a[][] = { { 1, 3, 4 }, { 2, 4, 3 }, { 3, 4, 5 } };
-        int b[][] = { { 1, 3, 4 }, { 2, 4, 3 }, { 1, 2, 4 } };
+        try (Scanner sc = new Scanner(System.in)) {
+            int a[][] = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            int b[][] = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 
-        int c[][] = new int[3][3];
+            int c[][] = new int[3][3];
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                c[i][j] = a[i][j] + b[i][j];
-                System.out.print(c[i][j] + " ");
+            System.out.println("Enter matrix a");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    a[i][j] = sc.nextInt();
+                }
+                System.out.println();
             }
+
+            System.out.println("Enter matrix b");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    b[i][j] = sc.nextInt();
+                }
+                System.out.println();
+            }
+
+            System.out.println("Matrix A");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(a[i][j] + " ");
+                }
+                System.out.println();
+            }
+
             System.out.println();
+
+            System.out.println("Matrix B");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(b[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println();
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    c[i][j] = a[i][j] + b[i][j];
+                    System.out.print(c[i][j] + " ");
+                }
+                System.out.println();
+            }
         }
     }
 }
