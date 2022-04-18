50fba594791859b6d759c982276b3ed94defab3a
Criz <crizmo595@gmail.com>
2022-04-18 15:18:23 +0530

    Update test.java

--- yeet/test.java
+++ yeet/test.java
@@ -3,5 +3,12 @@ package yeet;
 public class test {
     public static void main(String[] args) throws Exception {
         System.out.println("Criz !");
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
