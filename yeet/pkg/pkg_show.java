54aee53339e738497ed37f70f9007fd102239278
Criz <crizmo595@gmail.com>
2022-04-19 16:40:05 +0530

    Update pkg_show.java

--- yeet/pkg/pkg_show.java
+++ yeet/pkg/pkg_show.java
@@ -8,6 +8,7 @@ public class pkg_show {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter your marks: ");
             int score = sc.nextInt();
+
             pkg_main m = new pkg_main();
 
             System.out.println("Your grade is " + m.grading(score));
