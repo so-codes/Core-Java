95b258b755015d9dd9ec805419e49426809056dd
Criz <crizmo595@gmail.com>
2022-04-19 17:19:56 +0530

    Update swap.java

--- base/basics/swap.java
+++ base/basics/swap.java
@@ -1,16 +1,16 @@
 package base.basics;
 
 public class swap {
-    
+
     public static void main(String[] args) {
 
         int a = 5, b = 10, c;
-        System.out.print("\nThe two numbers are a = "+ a +" b = "+ b +" - Before swapping\n");
+        System.out.print("\nThe two numbers are a = " + a + " b = " + b + " - Before swapping\n");
 
-        c = a;  
-        a = b;  
-        b = c; 
+        c = a;
+        a = b;
+        b = c;
 
-        System.out.print("\nThe two numbers are a = "+ a +" b = "+ b +" - After swapping\n\n");
+        System.out.print("\nThe two numbers are a = " + a + " b = " + b + " - After swapping\n\n");
     }
 }
