21503149faa9ffad6bac58e2cde21ca16b4c5406
Criz <crizmo595@gmail.com>
2022-04-19 17:19:59 +0530

    Create bool.java

---
+++ yeet/bool.java
@@ -0,0 +1,21 @@
+package yeet;
+
+public class bool {
+
+    void wow() {
+        System.out.println("hi");
+    }
+
+    static bool bool = new bool();
+
+    public static void main(String[] args) {
+        bool.wow();
+    }
+
+    int a = 10;
+    float b = 5.5f;
+    double c = 5.5;
+    char d = 'a';
+    String e = "hi";
+
+}
