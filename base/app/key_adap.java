3e20f9b864a7300d14cb2f2f25060848db0aa33d
Criz <crizmo595@gmail.com>
2022-04-19 15:35:45 +0530

    Update key_adap.java

--- base/app/key_adap.java
+++ base/app/key_adap.java
@@ -9,36 +9,36 @@ public class key_adap extends KeyAdapter {
     JTextField area;
     JFrame f1;
 
-    key_adap(){
+    key_adap() {
         f1 = new JFrame();
         l1 = new JLabel("");
-        l1.setBounds(20,50,200,30);
+        l1.setBounds(20, 50, 200, 30);
         area = new JTextField(50);
-        area.setBounds(20,80,200,30);
+        area.setBounds(20, 80, 200, 30);
         area.addKeyListener(this);
 
         f1.add(l1);
         f1.add(area);
-        f1.setSize(300,300);
+        f1.setSize(300, 300);
         f1.setLayout(null);
         f1.setVisible(true);
         f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void keyTyped(KeyEvent e){
+    public void keyTyped(KeyEvent e) {
         l1.setText("Key typed: " + e.getKeyChar());
     }
 
-    public void keyPressed(KeyEvent e){
+    public void keyPressed(KeyEvent e) {
         l1.setText("Key pressed: " + e.getKeyChar());
     }
 
-    public void keyReleased(KeyEvent e){
+    public void keyReleased(KeyEvent e) {
         l1.setText("Key released: " + e.getKeyChar());
     }
 
     public static void main(String[] args) {
         new key_adap();
     }
-    
+
 }
