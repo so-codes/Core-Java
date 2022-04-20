a84330228922a9af13d749cf32f20a83339f3c7e
Criz <crizmo595@gmail.com>
2022-04-20 12:38:22 +0530

    Update main_inter.java

--- base/interface_bs/main_inter.java
+++ base/interface_bs/main_inter.java
@@ -8,14 +8,16 @@ interface A2 extends A1 {
     public void method2();
 }
 
-public class main_inter implements A2{
+public class main_inter implements A2 {
     public void method1() {
         System.out.println("method1");
     }
+
     public void method2() {
         System.out.println("method2");
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         main_inter m1 = new main_inter();
         m1.method1();
         m1.method2();
