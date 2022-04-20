120eb5f9bdc675185604ed6ec8c34f34ab29fbe8
Criz <crizmo595@gmail.com>
2022-04-20 12:38:27 +0530

    Update studInfo.java

--- base/interface_bs/studInfo.java
+++ base/interface_bs/studInfo.java
@@ -3,6 +3,7 @@ package base.interface_bs;
 public class studInfo {
     String sname = "";
     int sid = 0;
+
     void accept(String sname, int sid) {
         this.sname = sname;
         this.sid = sid;
@@ -17,6 +18,7 @@ class stud_info extends studInfo implements displayint {
     public void display() {
         System.out.println("Student name is " + sname + " and id is " + sid);
     }
+
     public static void main(String[] args) {
         stud_info s = new stud_info();
         s.accept("Pocky", 10);
